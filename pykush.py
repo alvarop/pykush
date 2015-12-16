8077b51ea752fc2a092450099088407a1cf4bcdf
Alvaro Prieto <alvaro@planet.com>
2015-12-16 09:18:03 -0800

    This seems to be working on ubuntu!

--- pykush.py
+++ pykush.py
@@ -4,49 +4,73 @@
 import usb.core
 import usb.util
 
-VENDOR_ID = 0x04D8
-PRODUCT_ID = 0xF2F7
-OLD_PRODUCT_ID = 0x0042
+class PYKUSH(object):
 
-dev = usb.core.find(idVendor=VENDOR_ID, idProduct=0xF2F7)
+	__VENDOR_ID = 0x04D8
+	__PRODUCT_ID = 0xF2F7
+	__OLD_PRODUCT_ID = 0x0042
 
-if dev is None:
-    raise ValueError('Device not found')
+	def __init__(self, serial_number=None):
+		self.serial_number = serial_number
 
-print('Device Connected!')
+		self.dev = usb.core.find(idVendor=self.__VENDOR_ID, idProduct=self.__PRODUCT_ID)
 
-dev.set_configuration(1)
+		if self.dev is None:
+		    raise ValueError('YKUSH not found')
 
-# get an endpoint instance
-cfg = dev.get_active_configuration()
-intf = cfg[(0,0)]
+		print('Device Connected!')
 
-dev.detach_kernel_driver(intf)
+		# Detach kernel driver if possible
+		try:
+			self.dev.detach_kernel_driver(0)
+		except usb.core.USBError:
+			print('Must already be detached...')
 
-print('Serial Number: ' + str(dev.serial_number))
+		self.dev.set_configuration()
 
-ep = usb.util.find_descriptor(
-    intf,
-    # match the first OUT endpoint
-    custom_match = \
-    lambda e: \
-        usb.util.endpoint_direction(e.bEndpointAddress) == \
-        usb.util.ENDPOINT_OUT)
+		# get an endpoint instance
+		cfg = self.dev.get_active_configuration()
+		intf = cfg[(0,0)]
 
-assert ep is not None
+		# print('Serial Number: ' + str(self.dev.serial_number))
 
-cmd = 0x1a  # All downstream ports UP
+		self.ep = usb.util.find_descriptor(
+		    intf,
+		    # match the first OUT endpoint
+		    custom_match = \
+		    lambda e: \
+		        usb.util.endpoint_direction(e.bEndpointAddress) == \
+		        usb.util.ENDPOINT_OUT)
 
-out_buff = [0] * 64
-out_buff[0] = cmd
+		assert self.ep is not None
 
-print('Attempting Write')
-# write the data
-ep.write(out_buff, timeout=5000)
+	def send_command(self ,command):
+		out_buff = [0] * 64
+		out_buff[0] = command
 
-print('Attempting Read')
-in_buff = ep.read(64, timeout=120000)
+		print('Attempting Write')
+		# write the data
+		self.ep.write(out_buff, timeout=5000)
 
-print(in_buff)
+		print('Attempting Read')
+		in_buff = self.ep.read(64, timeout=120000)
 
-dev.attach_kernel_driver(intf)
+	def enable_all(self):
+		self.send_command(0x1a)
+
+	def disable_all(self):
+		self.send_command(0x0a)
+
+	def enable(self, port):
+		if port < 1 or port > 3:
+			raise ValueError('Invalid port. Supported ports are: 1,2, and 3')
+
+		cmd = 0x10 | port
+		self.send_command(cmd)
+
+	def disable(self, port):
+		if port < 1 or port > 3:
+			raise ValueError('Invalid port. Supported ports are: 1,2, and 3')
+
+		cmd = 0x00 | port
+		self.send_command(cmd)
